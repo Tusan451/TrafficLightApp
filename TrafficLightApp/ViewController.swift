6af9c0ad2eac1cc71dad94972132b96bd7b17355
Oleg Fedotov <trevor706@gmail.com>
2022-07-29 10:34:36 +0300

    Coding logic of the main screen

--- TrafficLightApp/ViewController.swift
+++ TrafficLightApp/ViewController.swift
@@ -9,15 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var lights: [UIView]!
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
     @IBOutlet var startButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLight.alpha = 0.3
+        yellowLight.alpha = 0.3
+        greenLight.alpha = 0.3
+        
+        redLight.layer.cornerRadius = 110
+        yellowLight.layer.cornerRadius = 110
+        greenLight.layer.cornerRadius = 110
+        startButton.layer.cornerRadius = 10
+        
     }
 
     @IBAction func startButtonPressed() {
+        redLight.alpha = 1
+        startButton.setTitle("Next", for: .normal)
     }
     
 }
